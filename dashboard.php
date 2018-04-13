c03fe1a6024fe66551862c117fca2defa68f1fb8
Ziaf <Ziaf@users.noreply.github.com>
2018-04-13 17:28:37 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php
+require('db.php');
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dashboard - Secured Page</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>
+<p>Quizzes</p>
+<p><a href="Math.html">Math</a></p>
+<p><a href="index.php">science</a></p>
+<p><a href="index.php">Computer</a></p>
+</p>
+<p><a href="index.php">Home</a></p>
+<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
